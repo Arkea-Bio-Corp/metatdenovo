771e2face83d3a5712a8bf098ed40b5a04ba190f
Taylor Falk <tfalk@arkeabio.com>
2023-05-30 12:57:52 -0400

    CB-125 FastQC and Trim Galore and post-trim fastqc set up, need to change filenames on the fly for MultiQC to recognize them, I think

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -7,13 +7,13 @@ class WorkflowMain {
     //
     // Citation string for pipeline
     //
-    public static String citation(workflow) {
-        return "If you use ${workflow.manifest.name} for your analysis please cite:\n\n" +
-            "* The nf-core framework\n" +
-            "  https://doi.org/10.1038/s41587-020-0439-x\n\n" +
-            "* Software dependencies\n" +
-            "  https://github.com/${workflow.manifest.name}/blob/master/CITATIONS.md"
-    }
+    // public static String citation(workflow) {
+    //     return "If you use ${workflow.manifest.name} for your analysis please cite:\n\n" +
+    //         "* The nf-core framework\n" +
+    //         "  https://doi.org/10.1038/s41587-020-0439-x\n\n" +
+    //         "* Software dependencies\n" +
+    //         "  https://github.com/${workflow.manifest.name}/blob/master/CITATIONS.md"
+    // }
 
     //
     // Generate help string
